d25e180b096c940ee3038e89a153f675e14b7dfc
Zach Gawlik <zachgawlik@gmail.com>
2017-04-01 13:43:58 -0400

    Serve uploaded images

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path');
 const config = require('./config/config')[process.env.NODE_ENV || 'development'];
 const routes = require('./routes');
 
 const app = express();
 app.set('json spaces', 4);
 
+app.use('/image', express.static(path.join(__dirname, 'uploads')))
 app.use('/api', routes);
 
 app.listen(config.server.port, () => {
