945e5caa37f446598d3c8f8cc4942a499f81359b
Zach Gawlik <zachgawlik@gmail.com>
2017-04-19 16:28:58 -0400

    Get files relative to package root

--- utils/getSavedFilePath.js
+++ utils/getSavedFilePath.js
@@ -1,5 +1,8 @@
+const appRoot = require('app-root-path');
+const path = require('path');
+
 function getSavedFilePath(filenameWithExt) {
-  return `uploads/${filenameWithExt}`;
+  return path.resolve(appRoot.toString(), 'uploads', filenameWithExt);
 }
 
 module.exports = getSavedFilePath;
