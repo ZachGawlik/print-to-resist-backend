07599208c99ff4192a38175d12da43ebda28ee02
Zach Gawlik <zachgawlik@gmail.com>
2017-06-12 14:24:22 -0400

    Sort listings by most recent

---
+++ migrations/1497291347465_up_sorted_get_listings.sql
@@ -0,0 +1,23 @@
+USE print_to_resist;
+
+DROP PROCEDURE IF EXISTS get_approved_listings;
+DELIMITER //
+
+CREATE PROCEDURE get_approved_listings ()
+BEGIN
+  SELECT l.*,
+         (SELECT GROUP_CONCAT(image_id SEPARATOR ",")
+          FROM images
+          WHERE listing_id = l.listing_id) AS image_ids,
+         (SELECT GROUP_CONCAT(tag SEPARATOR ",")
+          FROM tags
+          WHERE listing_id = l.listing_id) AS tags,
+         (SELECT GROUP_CONCAT(url SEPARATOR ",")
+          FROM links
+          WHERE listing_id = l.listing_id) AS links
+  FROM listings l
+  WHERE l.is_approved = 1
+  ORDER BY ISNULL(deadline), deadline ASC, listing_id DESC;
+END//
+
+DELIMITER ;
